6ee0f5f8de0eec34370cea636e9dec7e6958065e
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2025-02-20 11:16:01 +0100

    Completed the protected routes

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//This route shows the registration and login web pages
-Route::get('/register',[AuthController::class,'showRegister'])->name('show.register');
-Route::get('/login',[AuthController::class,'showLogin'])->name('show.login');
 
-//this route handles registration and login process when the data is submitted
-Route::post('/register',[AuthController::class,'register'])->name('register');
-Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
-Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/ninjas/{ninja}',[NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/ninjas/{ninja}',[NinjaController::class, 'destroy'])->name('ninjas.destroy');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+//This route shows the registration and login web pages
+Route::get('/register','showRegister')->name('show.register');
+Route::get('/login','showLogin')->name('show.login');
+
+//this route handles registration and login process when the data is submitted
+Route::post('/register','register')->name('register');
+Route::post('/login','login')->name('login');
+});
+
+Route::middleware('auth')->controller(NinjaController::class)->group(function () {
+    Route::get('/ninjas',  'index')->name('ninjas.index');
+    Route::get('/ninjas/create', 'create')->name('ninjas.create');
+    Route::get('/ninjas/{ninja}',  'show')->name('ninjas.show');
+    Route::post('/ninjas',  'store')->name('ninjas.store');
+    Route::delete('/ninjas/{ninja}',  'destroy')->name('ninjas.destroy');
+});
+
 
